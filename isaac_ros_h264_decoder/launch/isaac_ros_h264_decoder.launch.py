5eb0586fda36798148cd38e5f33ac1baa12c253a
Aravind Sairam <asairam@lucidbots.com>
2024-08-16 13:49:16 +0200

    modified encoder and decoder for multiple topics

--- isaac_ros_h264_decoder/launch/isaac_ros_h264_decoder.launch.py
+++ isaac_ros_h264_decoder/launch/isaac_ros_h264_decoder.launch.py
@@ -22,17 +22,69 @@ from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
     """Launch the H.264 Decoder Node."""
-    decoder_node = ComposableNode(
-        name='decoder',
+    decoder_front_left = ComposableNode(
+        name='decoder_front_left',
         package='isaac_ros_h264_decoder',
-        plugin='nvidia::isaac_ros::h264_decoder::DecoderNode')
+        plugin='nvidia::isaac_ros::h264_decoder::DecoderNode',
+        remappings=[
+                    ('image_compressed', 'front_stereo_camera/left/image_compressed'),
+                    ('image_uncompressed', 'front_stereo_camera/left/image_uncompressed')
+        ])
+    
+    decoder_front_right = ComposableNode(
+        name='decoder_front_right',
+        package='isaac_ros_h264_decoder',
+        plugin='nvidia::isaac_ros::h264_decoder::DecoderNode',
+        remappings=[
+                    ('image_compressed', 'front_stereo_camera/right/image_compressed'),
+                    ('image_uncompressed', 'front_stereo_camera/right/image_uncompressed')
+        ])
+    
+    decoder_left_left = ComposableNode(
+        name='decoder_left_left',
+        package='isaac_ros_h264_decoder',
+        plugin='nvidia::isaac_ros::h264_decoder::DecoderNode',
+        remappings=[
+                    ('image_compressed', 'left_stereo_camera/left/image_compressed'),
+                    ('image_uncompressed', 'left_stereo_camera/left/image_uncompressed')
+        ])
+    
+    decoder_left_right = ComposableNode(
+        name='decoder_left_right',
+        package='isaac_ros_h264_decoder',
+        plugin='nvidia::isaac_ros::h264_decoder::DecoderNode',
+        remappings=[
+                    ('image_compressed', 'left_stereo_camera/right/image_compressed'),
+                    ('image_uncompressed', 'left_stereo_camera/right/image_uncompressed')
+        ])
+    
+
+    decoder_right_left = ComposableNode(
+        name='decoder_right_left',
+        package='isaac_ros_h264_decoder',
+        plugin='nvidia::isaac_ros::h264_decoder::DecoderNode',
+        remappings=[
+                    ('image_compressed', 'right_stereo_camera/left/image_compressed'),
+                    ('image_uncompressed', 'right_stereo_camera/left/image_uncompressed')
+        ])
+    
+    decoder_right_right = ComposableNode(
+        name='decoder_right_right',
+        package='isaac_ros_h264_decoder',
+        plugin='nvidia::isaac_ros::h264_decoder::DecoderNode',
+        remappings=[
+                    ('image_compressed', 'right_stereo_camera/right/image_compressed'),
+                    ('image_uncompressed', 'right_stereo_camera/right/image_uncompressed')
+        ])
 
     container = ComposableNodeContainer(
         name='decoder_container',
         namespace='',
         package='rclcpp_components',
         executable='component_container_mt',
-        composable_node_descriptions=[decoder_node],
+        composable_node_descriptions=[decoder_front_left, decoder_front_right,
+                                      decoder_left_left, decoder_left_right,
+                                      decoder_right_left, decoder_right_right],
         output='screen',
         arguments=['--ros-args', '--log-level', 'info']
     )
--- isaac_ros_h264_encoder/launch/isaac_ros_h264_encoder.launch.py
+++ isaac_ros_h264_encoder/launch/isaac_ros_h264_encoder.launch.py
@@ -27,16 +27,16 @@ def generate_launch_description():
     launch_args = [
         DeclareLaunchArgument(
             'input_height',
-            default_value='1200',
+            default_value='720',
             description='Height of the original image'),
         DeclareLaunchArgument(
             'input_width',
-            default_value='1920',
+            default_value='1080',
             description='Width of the original image'),
         DeclareLaunchArgument(
             'config',
             default_value='pframe_cqp',
-            description='Config of encoder'),
+            description='Config of encoder')
     ]
 
     # Encoder parameters
@@ -44,22 +44,98 @@ def generate_launch_description():
     input_width = LaunchConfiguration('input_width')
     config = LaunchConfiguration('config')
 
-    encoder_node = ComposableNode(
-        name='encoder',
+    encoder_front_left = ComposableNode(
+        name='encoder_front_left',
         package='isaac_ros_h264_encoder',
         plugin='nvidia::isaac_ros::h264_encoder::EncoderNode',
         parameters=[{
                 'input_height': input_height,
                 'input_width': input_width,
                 'config': config,
-        }])
+        }],
+        remappings=[
+                    ('image_raw', 'front_stereo_camera/left/image_raw'),
+                    ('image_compressed', 'front_stereo_camera/left/image_compressed')]
+        )
+    
+    encoder_front_right = ComposableNode(
+        name='encoder_front_right',
+        package='isaac_ros_h264_encoder',
+        plugin='nvidia::isaac_ros::h264_encoder::EncoderNode',
+        parameters=[{
+                'input_height': input_height,
+                'input_width': input_width,
+                'config': config,
+        }],
+        remappings=[
+                    ('image_raw', 'front_stereo_camera/right/image_raw'),
+                    ('image_compressed', 'front_stereo_camera/right/image_compressed')]
+        )
+    
+    encoder_left_left = ComposableNode(
+        name='encoder_left_left',
+        package='isaac_ros_h264_encoder',
+        plugin='nvidia::isaac_ros::h264_encoder::EncoderNode',
+        parameters=[{
+                'input_height': input_height,
+                'input_width': input_width,
+                'config': config,
+        }],
+        remappings=[
+                    ('image_raw', 'left_stereo_camera/left/image_raw'),
+                    ('image_compressed', 'left_stereo_camera/left/image_compressed')]
+        )
+    
+    encoder_left_right = ComposableNode(
+        name='encoder_left_right',
+        package='isaac_ros_h264_encoder',
+        plugin='nvidia::isaac_ros::h264_encoder::EncoderNode',
+        parameters=[{
+                'input_height': input_height,
+                'input_width': input_width,
+                'config': config,
+        }],
+        remappings=[
+                    ('image_raw', 'left_stereo_camera/right/image_raw'),
+                    ('image_compressed', 'left_stereo_camera/right/image_compressed')]
+        )
+    
+    encoder_right_left = ComposableNode(
+        name='encoder_right_left',
+        package='isaac_ros_h264_encoder',
+        plugin='nvidia::isaac_ros::h264_encoder::EncoderNode',
+        parameters=[{
+                'input_height': input_height,
+                'input_width': input_width,
+                'config': config,
+        }],
+        remappings=[
+                    ('image_raw', 'right_stereo_camera/left/image_raw'),
+                    ('image_compressed', 'right_stereo_camera/left/image_compressed')]
+        )
+    
+    encoder_right_right = ComposableNode(
+        name='encoder_right_right',
+        package='isaac_ros_h264_encoder',
+        plugin='nvidia::isaac_ros::h264_encoder::EncoderNode',
+        parameters=[{
+                'input_height': input_height,
+                'input_width': input_width,
+                'config': config,
+        }],
+        remappings=[
+                    ('image_raw', 'right_stereo_camera/right/image_raw'),
+                    ('image_compressed', 'right_stereo_camera/right/image_compressed')]
+        )
 
     container = ComposableNodeContainer(
         name='encoder_container',
         namespace='',
         package='rclcpp_components',
         executable='component_container',
-        composable_node_descriptions=[encoder_node],
+        composable_node_descriptions=[encoder_front_left, encoder_front_right,
+                                      encoder_left_left, encoder_left_right,
+                                      encoder_right_left, encoder_right_right],
         output='screen',
         arguments=['--ros-args', '--log-level', 'info']
     )
